58eba046d73895279a1324e5f873f1d90c1f624f
Anton Platonov <anton@platonov.us>
2022-01-30 20:12:58 -0500

    updated auction bid

--- contracts/AuctionBid.sol
+++ contracts/AuctionBid.sol
@@ -1,16 +1,16 @@
-pragma ton-solidity >=0.44.0;
+pragma ton-solidity >=0.52.0;
 pragma AbiHeader time;
 pragma AbiHeader pubkey;
 pragma AbiHeader expire;
 
 //================================================================================
 //
-contract AuctionBid
-{
-    //========================================
-    // Constants
-    address constant addressZero = address.makeAddrStd(0, 0);
+import "../interfaces/IBase.sol";
 
+//================================================================================
+//
+contract AuctionBid is IBase
+{
     //========================================
     // Error codes
     uint constant ERROR_MESSAGE_SENDER_IS_NOT_MY_OWNER = 100;
@@ -22,33 +22,39 @@ contract AuctionBid
     address static _bidderAddress;  // 
     uint128        _feeValue;       //
     uint256        _priceHash;      //
+    uint32         _dt;             //
     
     //========================================
     // Modifiers
-    function _reserve() internal inline view {    tvm.rawReserve(_feeValue, 0);    }
-    modifier  onlyAuction {    require(_auctionAddress != addressZero && _auctionAddress == msg.sender, ERROR_MESSAGE_SENDER_IS_NOT_MY_OWNER);    _; }
-    modifier  reserve     {   _reserve();    _;    }
+    modifier  onlyAuction {    require(_checkSenderAddress(_auctionAddress), ERROR_MESSAGE_SENDER_IS_NOT_MY_OWNER);    _; }
+    function _reserveFee() internal inline view {    tvm.rawReserve(_feeValue, 0);    }
+    modifier  reserveFee  {    if(msg.isInternal){ _reserve(); }    _;    }
 
     //========================================
     //
-    constructor(uint128 feeValue) public onlyAuction
+    constructor(uint128 feeValue) public onlyAuction reserveFee
     { 
         _feeValue = feeValue;
+        _dt       = now;
+
+        _bidderAddress.transfer(0, true, 128); // return change to bidder
     }
 
     //========================================
     //
-    function setPriceHash(uint256 priceHash) external onlyAuction
+    function setPriceHash(uint256 priceHash) external onlyAuction reserveFee returnChangeTo(_bidderAddress)
     {
         require(address(this).balance > _feeValue, ERROR_NOT_ENOUGH_MONEY);
-        _reserve();
         _priceHash = priceHash;
-        _bidderAddress.transfer(0, true, 128);
     }
 
     //========================================
     //
-    function revealPriceHash(uint128 price, uint256 salt) external view responsible onlyAuction returns (uint128 revealedPrice, uint256 revealedSalt, uint256 revealedHash, uint256 actualHash, address bidderAddress)
+    function revealPriceHash(uint128 price, uint256 salt) external view responsible onlyAuction returns (uint128 revealedPrice, 
+                                                                                                         uint256 revealedSalt, 
+                                                                                                         uint256 revealedHash, 
+                                                                                                         uint256 actualHash, 
+                                                                                                         address bidderAddress)
     {
         TvmBuilder builder;
         builder.store(price);
@@ -69,9 +75,13 @@ contract AuctionBid
 
     //========================================
     //
-    function getInfo() external view returns (address auctionAddress, address bidderAddress, uint128 feeValue, uint256 priceHash)
+    function getInfo() external view returns (address auctionAddress, 
+                                              address bidderAddress, 
+                                              uint128 feeValue, 
+                                              uint256 priceHash, 
+                                              uint32  dt)
     {
-        return(_auctionAddress, _bidderAddress, _feeValue, _priceHash);
+        return(_auctionAddress, _bidderAddress, _feeValue, _priceHash, _dt);
     }
 }
 
